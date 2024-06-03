dcba6de68c429d14832a6a162a7974a3f4ea1a30
Mark Tabatskyi <vasenka688@gmail.com>
2024-06-03 16:27:05 +0300

    added command class

--- Program.cpp
+++ Program.cpp
@@ -7,24 +7,54 @@
 #include <string.h>
 
 
+class ICommand 
+{
+public:
+    virtual ~ICommand() {}
+    virtual void Do() = 0;
+    virtual void Undo() = 0;
+};
+
+
+class Append : public ICommand
+{
+public:
+    Append();
+    ~Append();
+    void Do();
+    void Undo();
+private:
+    char** memory;
+};
+
+
 class Memory
 {
 public:
     Memory();
+    ~Memory();
+
+    int initializeMemory();
     void freeMemory();
+    int resizeLines();
+    int resizeLength();
+
     int currentLine;
     int currentLinesNum;
     int currentLenghNum;
     int currentLine;
 
+    char** textMemory;
+    ICommand* commandsMemory;
+
 private:
-    int initializeMemory();
     // make 3d array
-    // or array with action classes
+    // or array with command classes
+    /*char*** memory;
     char** memory0;
     char** memory1;
     char** memory2;
-    char** memory3;
+    char** memory3;*/
 };
 
 Memory::Memory()
@@ -32,51 +62,50 @@ Memory::Memory()
     int currentLine = 0;
     int currentLinesNum = 128;
     int currentLenghNum = 256;
-
-    
+    initializeMemory();
 };
+
+Memory::~Memory() 
+{
+    freeMemory();
+}
 
 void Memory::freeMemory()
 {
     for (int i = 0; i < currentLinesNum; i++)
     {
-        free(memory0[i]);
-        free(memory1[i]);
-        free(memory2[i]);
-        free(memory3[i]);
+        free(textMemory[i]);
     }
-    free(memory0);
-    free(memory1);
-    free(memory2);
-    free(memory3);
+    free(textMemory);
+    free(commandsMemory);
 }
 
 int Memory::initializeMemory()
 {
-    memory = (char**)malloc(currentLinesNum * sizeof(char*));
-    if (!memory)
+    textMemory = (char**)malloc(currentLinesNum * sizeof(char*));
+    if (!textMemory)
     {
         perror(">Memory allocation failed.\n");
         return 1;
     }
     for (int i = 0; i < currentLinesNum; i++)
     {
-        memory[i] = (char*)malloc(currentLenghNum * sizeof(char));
-        if (!memory[i])
+        textMemory[i] = (char*)malloc(currentLenghNum * sizeof(char));
+        if (!textMemory[i])
         {
             perror(">Memory allocation failed.\n");
             freeMemory();
             return 1;
         }
-        memory[i][0] = 0;
+        textMemory[i][0] = 0;
     }
 }
 
 
-int resizeLines()
+int Memory::resizeLines()
 {
     int newLinesNum = currentLinesNum * 2;
-    char** newMemory = (char**)realloc(memory, newLinesNum * sizeof(char*));
+    char** newMemory = (char**)realloc(textMemory, newLinesNum * sizeof(char*));
     if (!newMemory)
     {
         perror("Memory reallocation failed");
@@ -94,26 +123,26 @@ int resizeLines()
     }
 
     currentLinesNum = newLinesNum;
-    memory = newMemory;
+    textMemory = newMemory;
     printf("Line capacity expanded to %d\n", currentLinesNum);
     return 0;
 }
 
 
-int resizeLength()
+int Memory::resizeLength()
 {
     int newLengthNum = currentLenghNum * 2;
     for (int i = 0; i < currentLinesNum; i++)
     {
         char* newLine = (char*)malloc(newLengthNum * sizeof(char));
-        strcpy(newLine, memory[i]);
+        strcpy(newLine, textMemory[i]);
         if (!newLine)
         {
             perror("Memory reallocation failed for line resizing");
             return 1;
         }
-        memory[i] = (char*)malloc(newLengthNum * sizeof(char));
-        strcpy(memory[i], newLine);
+        textMemory[i] = (char*)malloc(newLengthNum * sizeof(char));
+        strcpy(textMemory[i], newLine);
         free(newLine);
     }
 
